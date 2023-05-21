d2c233f48cadc38deb9f14b33fb330cba064d8b2
Udbhav Srivastava <106414192+UdbhavSrivastava27@users.noreply.github.com>
2023-05-22 00:55:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+ if (digits === '') {
+    return [];
+  }
+
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  const combinations = [];
+
+  backtrack('', digits);
+
+  return combinations;
+
+  function backtrack(currentCombination, remainingDigits) {
+    if (remainingDigits.length === 0) {
+      combinations.push(currentCombination);
+      return;
+    }
+
+    const currentDigit = remainingDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const currentLetter = letters[i];
+      backtrack(currentCombination + currentLetter, remainingDigits.slice(1));
+    }
+  }
 }
 
 module.exports = letterCombinations;
